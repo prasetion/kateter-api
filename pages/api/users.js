1669497e005602cb87abbb2dacbe7cb27b36d387
prasetion <prasetion@gmail.com>
2022-01-15 19:09:30 +0700

    read and write json file

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,11 +1,35 @@
 const { join, resolve } = require("path");
-const { readFileSync } = require("fs");
+const { readFileSync, writeFileSync } = require("fs");
 
 export default async (req, res) => {
-  const templateDirectory = resolve(process.cwd(), "data");
-  const emailTemplate = readFileSync(
-    join(templateDirectory, "users.json"),
-    "utf8"
-  );
-  return res.status(200).json(emailTemplate);
+  if (req.method === "POST") {
+    var user = JSON.parse(req.body);
+
+    const newUser = {
+      id: Date.now(),
+      name: user.name,
+    };
+
+    console.log("POST :" + newUser);
+
+    const templateDirectory = resolve(process.cwd(), "data");
+    const users = readFileSync(join(templateDirectory, "users.json"), "utf8");
+    var usersData = JSON.parse(users);
+    usersData.users.push(newUser);
+
+    const replaceUsers = writeFileSync(
+      join(templateDirectory, "users.json"),
+      JSON.stringify(usersData)
+    );
+
+    console.log(usersData);
+
+    return res.status(200).json({
+      message: "success",
+    });
+  } else if (req.method === "GET") {
+    const templateDirectory = resolve(process.cwd(), "data");
+    const users = readFileSync(join(templateDirectory, "users.json"), "utf8");
+    return res.status(200).json(users);
+  }
 };
