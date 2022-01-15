7a0a3c0ccc3e292ca764b4f3c91d64fa352f6954
prasetion <prasetion@gmail.com>
2022-01-15 19:42:29 +0700

    changing route

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,36 +1,16 @@
-const { join, resolve } = require("path");
-const { readFileSync, writeFileSync } = require("fs");
-// import {users} from "../../data/users.json"
-export default async (req, res) => {
-  if (req.method === "POST") {
-    var user = JSON.parse(req.body);
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {users} from "./data/users"
 
+export default function handler(req, res) {
+  if(req.method === "GET"){
+    res.status(200).json(users);
+  }else if(req.method === "POST"){
+    const user = JSON.parse(req.body);
     const newUser = {
-      id: Date.now(),
-      name: user.name,
-    };
-
-    console.log("POST :" + newUser);
-
-    const templateDirectory = resolve(process.cwd(), "data");
-    const users = readFileSync(join(templateDirectory, "users.json"), "utf8");
-    var usersData = JSON.parse(users);
-    usersData.users.push(newUser);
-
-    // save overwrite
-    // const replaceUsers = writeFileSync(
-    //   join(templateDirectory, "users.json"),
-    //   JSON.stringify(usersData)
-    // );
-
-    return res.status(200).json(usersData);
-
-    // return res.status(200).json({
-    //   message: "success",
-    // });
-  } else if (req.method === "GET") {
-    const templateDirectory = resolve(process.cwd(), "data");
-    const users = readFileSync(join(templateDirectory, "users.json"), "utf8");
-    return res.status(200).json(users);
+      id:Date.now(),
+      name:user.name
+    }
+    users.data.push(newUser);
+    res.status(200).json(users)
   }
-};
+}
