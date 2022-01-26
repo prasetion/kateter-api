d5d754ee3e6fb24843b5f08649a241a7393e4a9f
prasetion <prasetion@gmail.com>
2022-01-26 13:08:54 +0700

    add request header cors

--- pages/api/simulation.js
+++ pages/api/simulation.js
@@ -20,6 +20,17 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 export default async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
   if (req.method === "GET") {
     const simulationCollection = collection(db, "simulations");
     const simulationSnapshot = await getDocs(simulationCollection);
