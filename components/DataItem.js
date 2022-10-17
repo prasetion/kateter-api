971e8dd548d5fdd20cde8176cde0a1266340d8ac
prasetion <prasetion@gmail.com>
2022-10-17 14:52:27 +0700

    new stats for simulation data

--- components/DataItem.js
+++ components/DataItem.js
@@ -5,13 +5,18 @@ const DataItem = ({ item }) => {
   return (
     <div>
       <div className={styles.card}>
-        <h3>NRP: {item.nrp}</h3>
+        <h3>NRP: {item.studentId}</h3>
         <p>
           Nama: {item.name} <br />
-          Kelas: {item.classroom} <br />
-          Time: {item.time} <br />
-          Correct: {item.correct} <br />
-          Wrong: {item.correct} <br />
+          Institusi: {item.school} <br />
+          Email: {item.email} <br />
+          Jenis Simulasi: {item.simulationType} <br />
+          Waktu Mulai : {item.startTime} <br />
+          Waktu Akhir : {item.endTime} <br />
+          Durasi: {item.duration} <br />
+          Langkah Benar: {item.correctStep} <br />
+          Langkah Salah: {item.wrongStep} <br />
+          Score: {item.score} <br />
         </p>
       </div>
     </div>
