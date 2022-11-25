bef77cff42d44cf7eca4a4918d88d13f30be85a9
prasetion <prasetion@gmail.com>
2022-11-25 10:24:04 +0700

    adding loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function Loading() {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    router.events.on("routeChangeStart", (url) => {
+      console.log("routing is start");
+      setLoading(true);
+    });
+    router.events.on("routeChangeComplete", (url) => {
+      console.log("routing is complete");
+      setLoading(false);
+    });
+  });
+
+  return (
+    loading && (
+      <div className="spinner-wrapper">
+        <div className="spinner"></div>
+      </div>
+    )
+  );
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Loading />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
